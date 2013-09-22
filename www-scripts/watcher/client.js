d01ee87e9e80ac0ff55c6d41981b17433b75436e
Marcus Bergman <mraxus@gmail.com>
2013-09-23 00:49:10 +0200

    Added watcher client global functions for connection/disconnection/mouse movements.

--- www-scripts/watcher/client.js
+++ www-scripts/watcher/client.js
@@ -10,5 +10,31 @@
 
  */
 
-
 console.log('socket.io is not enabled.');
+
+function x_connectClient (id) {
+	x_addCursor(id);
+}
+function x_disconnectClient (id) {
+	x_removeCursor(id);
+}
+
+// Add cursor
+function x_addCursor (id) {
+	try {
+		var style = 'position: absolute; left: 0px; top: 0px;';
+		var img = '<img id="' + id + '" class="cursor" src="/index_files/cursor.png" style="' + style + '" />';
+		$('#content').append(img);
+		return true;
+	} catch(err) {
+		return false;
+	}
+}
+function x_moveCursor(id, left, top, interval) {
+	$('.cursor#' + id).animate({left: left + 'px', top: top + 'px'}, interval);
+	return true;
+}
+function x_removeCursor (id) {
+	$('.cursor#' + id).remove();
+	return true;
+}
