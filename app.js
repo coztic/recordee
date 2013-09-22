904279a4955bd173568e481b309fee79a28bdc1a
Marcus Bergman <mraxus@gmail.com>
2013-09-22 15:44:32 +0200

    app.js: Added reference to lib/server.js with setup call to initiate socket communication.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+var path = require('path');
 var express = require('express');
 var app = express.createServer();
 var io = require('socket.io').listen(app);
-var path = require('path');
+var IoServer = require(path.join(__dirname, 'lib', 'server'));
+var ioServer = new IoServer(io);
 
 app.configure(function () {
 	app.use(
@@ -15,15 +17,4 @@ app.configure(function () {
 });
 
 app.listen(80);
-//io.sockets.on('connection', function (socket) {
-//	socket.on('setPseudo', function (data) {
-//		socket.set('pseudo', data);
-//	});
-//	socket.on('message', function (message) {
-//		socket.get('pseudo', function (error, name) {
-//			var data = { 'message' : message, pseudo : name };
-//			socket.broadcast.emit('message', data);
-//			console.log("user " + name + " send this : " + message);
-//		})
-//	});
-//});
+ioServer.setup();
