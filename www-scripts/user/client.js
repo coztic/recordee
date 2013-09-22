6186a0b9dad6246f79080b35e43addd6c46d9238
Marcus Bergman <mraxus@gmail.com>
2013-09-22 18:59:43 +0200

    Moved client.js to www-scripts/user.

---
+++ www-scripts/user/client.js
@@ -0,0 +1,29 @@
+/*
+
+ ##     ##  ######  ######## ########
+ ##     ## ##    ## ##       ##     ##
+ ##     ## ##       ##       ##     ##
+ ##     ##  ######  ######   ########
+ ##     ##       ## ##       ##   ##
+ ##     ## ##    ## ##       ##    ##
+ #######   ######  ######## ##     ##
+
+ */
+
+var socket = io.connect();
+
+socket.on('info-request', function (data) {
+
+	// send user agent info to server
+	var userInfo = { userAgent: navigator.userAgent };
+	socket.emit('info-response', userInfo);
+});
+
+
+// SETUP MOUSE MOVE LISTENER
+
+$('body').mousemove(function ( event ) {
+	var pageCoords = "( " + event.pageX + ", " + event.pageY + " )";
+	var clientCoords = "( " + event.clientX + ", " + event.clientY + " )";
+	console.log( "( event.pageX, event.pageY ) : " + pageCoords );
+});
