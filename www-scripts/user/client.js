22adfe3cb0f02833c21a2e313656c120b5af808f
Marcus Bergman <mraxus@gmail.com>
2013-09-22 23:22:02 +0200

    Added user client script for mouse movements.

--- www-scripts/user/client.js
+++ www-scripts/user/client.js
@@ -11,19 +11,38 @@
  */
 
 var socket = io.connect();
+var data = {
+	mouse: {
+		interval: 125,
+		timer: null,
+		left: '0px',
+		top: '0px'
+	}
+};
 
 socket.on('info-request', function (data) {
 
 	// send user agent info to server
-	var userInfo = { userAgent: navigator.userAgent };
+	var userInfo = { command: 'connect', data: navigator.userAgent };
 	socket.emit('info-response', userInfo);
 });
 
-
 // SETUP MOUSE MOVE LISTENER
+$('body').mousemove(function (event) {
+	data.mouse.left = event.pageX;
+	data.mouse.top = event.pageY;
+	//console.log( "Set new mouse coords" );
 
-$('body').mousemove(function ( event ) {
-	var pageCoords = "( " + event.pageX + ", " + event.pageY + " )";
-	var clientCoords = "( " + event.clientX + ", " + event.clientY + " )";
-	console.log( "( event.pageX, event.pageY ) : " + pageCoords );
+	var fn = function() {
+		var m = data.mouse;
+		var query = { command: 'mousemove', data: {left: m.left, top: m.top, interval: m.interval} };
+		socket.emit('mousemove', query);
+		data.mouse.timer = null;
+		console.log( "Emitted mousemove, cleared timer" );
+	};
+
+	if (!data.mouse.timer) {
+		data.mouse.timer = setTimeout(fn, data.mouse.interval);
+		//console.log( "Set new timer" );
+	}
 });
