6c9cca10706d34dcb18c22a64ba07d9d5387dbf7
Marcus Bergman <mraxus@gmail.com>
2013-09-22 18:40:33 +0200

    app.js: Removed appBase functionality. Now starting two apps: 80 with web sockets enabled and 8080 without web sockets.

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
 var path = require('path');
-var express = require('express');
-var app = express.createServer();
-var io = require('socket.io').listen(app);
-var IoServer = require(path.join(__dirname, 'lib', 'server'));
-var ioServer = new IoServer(io);
+var base = require(path.join(__dirname, 'lib', 'appBase'));
 
-app.configure(function () {
-	app.use(
-		'/',
-		express.static(path.join(__dirname, '..', 'www'))
-	);
-	app.use(
-		'/scripts/',
-		express.static(path.join(__dirname, 'lib'))
-	);
-});
-
-app.listen(80);
-ioServer.setup();
+base.setup(80, true);
+base.setup(8080, false);
+//app.listen(80);
+//ioServer.setup();
